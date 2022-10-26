c3e497d9e02db6e1560c015e0d082ba0ccbb0f05
Esther Maina <mainaesther77@gmial.com>
2022-10-26 10:12:33 +0300

    Completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+    #sit instance
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark
+
+fido.sit
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    # Class body
+  
+    # Instance Method Definition
+    def talk
+      puts "Hello World!"
+    end
+    #sit instance
+    def walk 
+        puts "The Person is walking"
+    end
+end
+
